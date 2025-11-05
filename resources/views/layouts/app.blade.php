26239ff58af7cf77d35edcef3d42e0c9fdc28b5e
Dorian Mayamba <dorian.mayamba@gmail.com>
2025-11-05 01:34:21 +0000

    fixed typo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
 <body>
     <nav class="navbar navbar-expand-lg fixed-top">
         <div class="container">
-            <a class="navbar-brand fw-bold text-primary" href="{{ url('/') }}">{{ config('app.name') }}/a>
+            <a class="navbar-brand fw-bold text-primary" href="{{ url('/') }}">{{ config('app.name') }}</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
